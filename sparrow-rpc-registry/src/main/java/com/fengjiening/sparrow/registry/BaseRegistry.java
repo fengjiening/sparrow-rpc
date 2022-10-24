2d6ea4d21b26b575a1aebaa88a9237813096148b
fengjiening <fengjnk@yonyou.com>
2022-10-24 16:02:12 +0800

    模块建立-fix models name

---
+++ sparrow-rpc-registry/src/main/java/com/fengjiening/sparrow/registry/BaseRegistry.java
@@ -0,0 +1,11 @@
+package com.fengjiening.sparrow.registry;
+
+/**
+ * @ClassName: BaseRegistry
+ * @Description: TODO
+ * @Date: 2022/10/24 15:56
+ * @Author: fengjiening::joko
+ * @Version: 0.0
+ */
+public interface BaseRegistry {
+}
---
+++ sparrow-rpc-registry/src/main/java/com/fengjiening/sparrow/registry/RegistryContext.java
@@ -0,0 +1,10 @@
+package com.fengjiening.sparrow.registry;
+/**
+ * @ClassName: RegistryContext
+ * @Description: 注册中心上下文
+ * @Date: 2022/10/24 15:56
+ * @Author: fengjiening::joko
+ * @Version: 0.0
+ */
+public class RegistryContext {
+}
